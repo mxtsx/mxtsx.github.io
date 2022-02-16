715428b27dd5865927ed35e2d002359067699c7d
mxtsx <doppagefnt@gmail.com>
2022-02-16 21:43:27 +0100

    Hamburger button was added

---
+++ components/burger-button.component.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface IProps {
+    isOpen: boolean,
+    setIsOpen: (isOpen: boolean) => void
+}
+
+export const BurgerButton: React.FC<IProps> = ({isOpen, setIsOpen}) => {
+    const genericHamburgerLine = `h-1 w-8 my-1 rounded-full bg-black transition ease transform duration-300`;
+
+    return (
+        <button
+            className="md:hidden flex flex-col h-9 w-9 justify-center items-center group"
+            onClick={() => setIsOpen(!isOpen)}
+        >
+            <div
+                className={`${genericHamburgerLine} ${
+                    isOpen
+                        ? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
+                        : "opacity-50 group-hover:opacity-100"
+                }`}
+            />
+            <div
+                className={`${genericHamburgerLine} ${
+                    isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+                }`}
+            />
+            <div
+                className={`${genericHamburgerLine} ${
+                    isOpen
+                        ? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
+                        : "opacity-50 group-hover:opacity-100"
+                }`}
+            />
+        </button>
+    );
+};
--- components/header.component.tsx
+++ components/header.component.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Select} from "./select.component";
 import {useTypedSelector} from "../hooks/useTypedSelector";
 import Link from "next/link";
 import {Switch} from "./switch.component";
+import {BurgerButton} from "./burger-button.component";
 
 export const HeaderComponent: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     const {navbarText} = useTypedSelector(state => state.header)
     const {language} = useTypedSelector(state => state.language)
+
     return (
         <header className={'flex flex-col justify-between mx-auto p-7 md:flex-row md:items-center max-w-7xl shadow-md'}>
             <div className={'flex flex-col w-full justify-between items-center space-y-4 xs:space-y-0 xs:flex-row'}>
@@ -17,9 +21,10 @@ export const HeaderComponent: React.FC = () => {
                 <div className={'flex items-center space-x-7 md:ml-auto'}>
                     <Select />
                     <Switch />
+                    <BurgerButton isOpen={isOpen} setIsOpen={setIsOpen} />
                 </div>
             </div>
-            <div className={`flex flex-col items-center justify-center space-y-4 mt-4 md:space-y-0 md:mt-0 md:flex-row  md:space-x-7 md:ml-8`}>
+            <div className={`${!isOpen ? 'hidden' : 'inline-flex'} flex flex-col items-center justify-center space-y-4 mt-7 transition ease transform duration-300 md:inline-flex md:space-y-0 md:mt-0 md:flex-row  md:space-x-7 md:ml-8`}>
                 {navbarText.map(el => <Link key={el.id} href={el.path}><h3 className={'header-link whitespace-nowrap'}>{el.title[language]}</h3></Link>)}
             </div>
         </header>

400f94290ee5f1ac58b81da8f7ea1aaf0f8647d9
mxtsx <doppagefnt@gmail.com>
2022-02-16 11:20:12 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Main</title>
+      </Head>
+      Page will be here
+    </div>
+  )
+}

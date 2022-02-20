85735c663469afc431b7da61f1cb31cf9551e5a9
mxtsx <doppagefnt@gmail.com>
2022-02-20 15:58:58 +0100

    unusable action creator was deleted

--- store/action-creators/header/header.action-creators.ts
+++ store/action-creators/header/header.action-creators.ts
@@ -2,5 +2,4 @@ import {HeaderEnums} from "../../../types/header.types";
 
 export const headerActions = {
     pageWasSelected: (page: string) => ({type: HeaderEnums.SET_SELECTED_PAGE, payload: {page}} as const),
-    isLoadingSet: (isLoading: boolean) => ({type: HeaderEnums.SET_IS_LOADING, payload: {isLoading}} as const),
 }

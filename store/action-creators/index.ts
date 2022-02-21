22fc1b931aebbfc6f9c7c09fa940c793f44fd047
mxtsx <doppagefnt@gmail.com>
2022-02-21 14:22:04 +0100

    header thunks was deleted from index thunks file

--- store/action-creators/index.ts
+++ store/action-creators/index.ts
@@ -1,11 +1,9 @@
 import * as AboutThunks from "./about/about.thunks"
-import * as HeaderThunks from "./header/header.thunks"
 import * as HomepageThunks from "./homepage/homepage.thunks"
 import * as PortfolioThunks from "./portfolio/portfolio.thunks"
 
 export default {
     ...AboutThunks,
-    ...HeaderThunks,
     ...HomepageThunks,
     ...PortfolioThunks
 }
